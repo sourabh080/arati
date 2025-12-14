0b65562e22cfec1b33711b819310bd6e5ac2d7e3
Arati <sourabhvmore@gmail.com>
2025-12-14 21:04:46 +0530

    reverse stirng

---
+++ javaCodesgit/src/javaCodes/revString.java
@@ -0,0 +1,24 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        // Input string
+        String input = "Hello, World!";
+
+        // Reverse the string using StringBuilder
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        // Output the reversed string
+        System.out.println("Reversed String: " + reversed);
+    }
+}
+public class ReverseString {
+    public static void main(String[] args) {
+        // Input string
+        String input = "Hello, World!";
+
+        // Reverse the string using StringBuilder
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        // Output the reversed string
+        System.out.println("Reversed String: " + reversed);
+    }
+}
